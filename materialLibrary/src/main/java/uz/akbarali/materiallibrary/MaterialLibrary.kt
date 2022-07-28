7784422b8ce33ea44bb51aaf181719964db7b8c1
akbar <akbaralixasanov2000@gmail.com>
2022-07-28 13:30:22 +0500

    Hi

--- materialLibrary/src/main/java/uz/akbarali/materiallibrary/MaterialLibrary.kt
+++ materialLibrary/src/main/java/uz/akbarali/materiallibrary/MaterialLibrary.kt
@@ -1,12 +1,11 @@
 package uz.akbarali.materiallibrary
 
-import android.app.AlertDialog
-import android.app.Dialog
 import android.content.Context
+import android.content.DialogInterface
 import android.widget.Toast
-import androidx.core.content.contentValuesOf
+import androidx.fragment.app.FragmentManager
+import com.google.android.material.datepicker.MaterialDatePicker
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import com.google.android.material.dialog.MaterialDialogs
 
 class MaterialLibrary() {
     fun materialDialog(
@@ -27,7 +26,35 @@ class MaterialLibrary() {
             .setPositiveButton(positiveButton) { dialog, which ->
                 Toast.makeText(context, "${dialog}", Toast.LENGTH_SHORT).show()
                 dialog.dismiss()
-            }
+            }.show()
     }
+
+    fun materialCheckbox(
+        context: Context
+    ) {
+        val animals = arrayOf("Twitter", "Google", "Instagram", "Facebook")
+        val checkedItems = booleanArrayOf(false, true, false, true)
+        MaterialAlertDialogBuilder(context)
+            .setTitle("Social Networks")
+            .setMultiChoiceItems(
+                animals,
+                checkedItems
+            ) { p0, which, isChecked ->
+                return@setMultiChoiceItems
+            }
+            .setPositiveButton("Choose") { dialog, which ->
+                Toast.makeText(context, "${dialog}", Toast.LENGTH_SHORT).show()
+                dialog.dismiss()
+            }.show()
+    }
+
+    fun materialDatePicker(getSupportFragmentManager: FragmentManager) {
+        MaterialDatePicker.Builder.datePicker()
+            .setTitleText("SELECT A DATE")
+            .build()
+            .show(getSupportFragmentManager, "DATE_PICKER_RANGE")
+    }
+
+
 
 }
